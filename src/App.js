8ca3c9df5980adcb529f0e73bbed45d6c671b016
anferoar96 <afromana@unal.edu.co>
2019-05-19 22:01:13 -0500

    Probando, bootstrap y clases

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
-import TestBPMN from './TestBPMN.js';
+import TestBPMN from './components/TestBPMN.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          PROCESO DE MOVILIDAD V0.0.0.1
-        </h1>
-        <TestBPMN/>
+class App extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            PROCESO DE MOVILIDAD V0.0.0.1
+          </h1>
+          <TestBPMN/>
 
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    );
+  }
+  }
+
 
 export default App;
