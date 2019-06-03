190911c587f36078b184483e846d059bd6b38f2a
anferoar96 <afromana@unal.edu.co>
2019-06-03 18:13:22 -0500

    Pagina principal full y mas cambios

---
+++ src/components/ConvEst.js
@@ -0,0 +1,17 @@
+//Del otro grupo
+import React from "react";
+import meme from '../assets/memesoft.jpg'
+
+
+const ConvEst =()=> {
+  return(
+    <div className="container">
+      <h4 className="center">Convocatoria de estudiantes</h4>
+      <br/>
+      <img src={meme} alt=""/>
+    </div>
+  );
+
+}
+
+export default ConvEst;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom'
 
 const Navbar =()=> {
   return(
-    <nav class="nav-wrapper grey darken-3">
-        <div class="container" >
+    <nav className="nav-wrapper grey darken-3">
+        <div className="container" >
             <a className='brand-logo'>Intercambio UN</a>
-            <ul class="right" >
+            <ul className="right" >
+                <li><Link to='/test'>Pruebas</Link></li>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/info'>Informacion</Link></li>
+                <li><Link to='/convocatoria'>Convocatoria</Link></li>
                 <li>Ingresar</li>
               </ul>
           </div>
